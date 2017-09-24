a871e4b089be0c8a45b11d945254002fc5975f4d
Pavel Krasnikov <pasha.gooff@gmail.com>
2017-09-24 16:58:55 +0300

    Add ping

--- main.go
+++ main.go
@@ -1,5 +1,6 @@
 package main
 
+// Библиотеки, нужные программе
 import (
 	"log"
 
@@ -7,6 +8,7 @@ import (
 )
 
 func main() {
+	// подключаемся к боту с помощью токена
 	bot, err := tgbotapi.NewBotAPI("MyAwesomeBotToken")
 	if err != nil {
 		log.Panic(err)
@@ -15,22 +17,32 @@ func main() {
 	bot.Debug = true
 
 	log.Printf("Authorized on account %s", bot.Self.UserName)
-
+	// инициализируем канал, куда будут прилетать обновления от API
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
 
 	updates, err := bot.GetUpdatesChan(u)
-
+	// читаем обновления из канала
 	for update := range updates {
 		if update.Message == nil {
 			continue
 		}
+		// Созадаем сообщение
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		//Ping-pong
+		if update.Message.Text == "/ping" {
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, "pong")
+			// В остальных случаях
+		} else if update.Message != nil {
+			// Ответить на сообщение его копией
+			msg = tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
 
-		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, update.Message.Text)
+		// log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+		// Созадаем сообщение
+
 		msg.ReplyToMessageID = update.Message.MessageID
-
 		bot.Send(msg)
 	}
 }
